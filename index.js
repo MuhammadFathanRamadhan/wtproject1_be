abc603dec1d21dff7409b5050ae10cb6b70f5e37
NothingFT <muhammad.fathan.ramadhan@gmail.com>
2026-02-12 20:45:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware agar server bisa membaca JSON (penting untuk kirim data dari Flutter)
+app.use(express.json());
+
+// Endpoint percobaan (Bisa diakses dari browser atau Flutter)
+app.get('/', (req, res) => {
+    res.send({
+        message: "Server Node.js sudah berjalan!",
+        status: "success"
+    });
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+    console.log(`Server aktif di http://localhost:${PORT}`);
+});
